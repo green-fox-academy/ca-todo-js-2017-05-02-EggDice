23de48613ce7bd32b6dc886842982ecf81660427
Tamas Kokeny <tamas.kokeny.87@gmail.com>
2017-05-02 16:21:51 +0200

    feat(usage): prints usage on run
    It prints the usage regardless the arguments
    
    closes #1

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var usage = `NodeJS Todo application
+=======================
+
+  Command line arguments:
+ -l   Lists all the tasks
+ -a   Adds a new task
+ -r   Removes an task
+ -c   Completes an task
+`;
+
+console.log(usage);
